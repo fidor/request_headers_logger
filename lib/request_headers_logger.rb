abc096106d006b925c484c5a7f36082b32dc9c32
Al-waleed shihadeh <shihadeh@fidor.de>
2018-03-21 11:12:10 +0100

    use son and text formatters instead of tagged logging

--- lib/request_headers_logger.rb
+++ lib/request_headers_logger.rb
@@ -1,37 +1,50 @@
 # frozen_string_literal: true
 
-require 'active_support'
 require 'request_headers_middleware'
+require 'request_headers_logger/configuration'
+require 'request_headers_logger/json_formatter'
+require 'request_headers_logger/text_formatter'
 require 'request_headers_logger/delayed_job/delayed_job'
 
 module RequestHeadersLogger # :nodoc:
   extend self
 
   attr_accessor :whitelist
-  @whitelist = ['x-request-id'.to_sym]
+  @whitelist     = ['x-request-id'.to_sym]
+  @configuration = Configuration.new
+
+  def configure
+    yield @configuration
+
+    prepare_loggers
+  end
 
   def tags
     filter(RequestHeadersMiddleware.store)
   end
 
-  def tag_logger(logger)
-    logger = tagged_logger(logger) unless logger.respond_to? :push_tags
-    tags.each do |_tag, value|
-      logger.push_tags(value) unless value.nil?
-    end
+  def log_format
+    @configuration.log_format
   end
 
-  def untag_logger(logger)
-    logger = tagged_logger(logger) unless logger.respond_to? :pop_tags
-    tags.each do |_tag, value|
-      logger.pop_tags unless value.nil?
-    end
+  def loggers
+    @configuration[:loggers]
   end
 
   private
 
-  def tagged_logger(logger)
-    ActiveSupport::TaggedLogging.new(logger)
+  def prepare_loggers
+    loggers.each do |logger|
+      logger_formatter logger
+    end
+  end
+
+  def logger_formatter(logger)
+    logger.tap { |obj| obj.formatter = formatter_class.new }
+  end
+
+  def formatter_class
+    RequestHeadersLogger.const_get("#{log_format.capitalize}Formatter")
   end
 
   def filter(store)
