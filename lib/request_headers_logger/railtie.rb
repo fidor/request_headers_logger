92c58eace96303b71812d33ecb4db621e381f589
Al-waleed shihadeh <shihadeh@fidor.de>
2018-12-19 11:28:39 +0100

    extend rails logger only for djs

--- lib/request_headers_logger/railtie.rb
+++ lib/request_headers_logger/railtie.rb
@@ -5,8 +5,10 @@ module RequestHeadersLogger
     # rubocop:disable Metrics/BlockLength
     config.after_initialize do
       RequestHeadersLogger.logger_formatter(MessageQueue.logger) if defined?(MessageQueue)
-      RequestHeadersLogger.logger_formatter(Delayed::Worker.logger) if defined?(Delayed)
-      RequestHeadersLogger.logger_formatter(Rails.logger)
+      if defined?(Delayed)
+        RequestHeadersLogger.logger_formatter(Delayed::Worker.logger)
+        RequestHeadersLogger.logger_formatter(Rails.logger)
+      end
     end
     # rubocop:enable Metrics/BlockLength
   end
